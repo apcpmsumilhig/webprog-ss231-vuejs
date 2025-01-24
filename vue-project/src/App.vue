5080c3e3e4b8e59599c191b43b4dfdbff60f87c0
apcpmsumilhig <pmsumilhig@student.apc.edu.ph>
2025-01-24 09:14:40 +0800

    output check for personal profile component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,10 +2,12 @@
 
 <template>
    <h1>Food</h1>
+   <personal-profile/><br>
   <food-item/> <br>
   <food-item2/> <br>
   <food-item/>  <br>
   <food-item2/> <br>
+  
 </template>
 
 <style></style>
