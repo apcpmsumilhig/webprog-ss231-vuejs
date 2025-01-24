a27ef41b902d7b2efbad8ad30d5df677219a5ebd
apcpmsumilhig <pmsumilhig@student.apc.edu.ph>
2025-01-24 08:26:38 +0800

    check

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -7,4 +7,12 @@
 
 <script></script>
 
-<style></style>
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
