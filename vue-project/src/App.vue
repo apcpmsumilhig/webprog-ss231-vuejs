48b72cf8a075f8374a73d93244ee713121bf4451
apcpmsumilhig <pmsumilhig@student.apc.edu.ph>
2025-02-06 14:28:40 +0000

    Updated for comment and Comment Form

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,31 +1,13 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
-</script>
-
 <template>
-
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
-
   <h1>Food</h1>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <personal-profile/>
+  <country/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
 
-
-
-<style></style>
+<script></script>
